01ff398e55a5d51cf31487f63ead95a4f6dba77a
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-04-23 16:27:17 -0300

    Calculate fitness function for every individual

--- GA_ChuBeasley.py
+++ GA_ChuBeasley.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+
+def fitness(crom, n, x1_l, x1_h, x2_l, x2_h):
+
+    import numpy as np
+
+    x1 = (crom[0]/(10.**n))*(x1_h - x1_l) + x1_l
+    x2 = (crom[1]/(10.**n))*(x2_h - x2_l) + x2_l
+
+    print x1, x2
+
+    return x1*np.sin(10*np.pi*x1) + x2*np.cos(3*np.pi*(x2**2))
+
+
 print type(bin(10))
 
 # Limits of x
@@ -19,12 +32,24 @@ gen_max = 10
 # Current generation
 gen = 0
 
+# Precision
+digits = 6
+
+# List of individuals
 specimens = []
 
+# Creating unique individuals
 for i in range(pop_size):
-    a = np.random.randint(10.**6)
-    b = np.random.randint(10.**6)
+    a = np.random.randint(10.**digits)
+    b = np.random.randint(10.**digits)
+
+    # This loop prevents creating repeated individuals
     while [0, (a, b)] in specimens:
-        a = np.random.randint(10.**6)
-        b = np.random.randint(10.**6)
+        a = np.random.randint(10.**digits)
+        b = np.random.randint(10.**digits)
+
+    # Add new individual to population
     specimens.append([0, (a, b)])
+
+for spec in specimens:
+    spec[0] = fitness(spec[1], digits, xmin, xmax, ymin, ymax,)
