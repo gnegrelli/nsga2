fb0b21780799fc166adbdbb438d6e022a9a5d49d
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-04-24 16:59:14 -0300

    Change decimal to binary notation
    With this notation there's no possibility of creating a child outside the region of x and y

--- GA_ChuBeasley.py
+++ GA_ChuBeasley.py
@@ -6,8 +6,8 @@ def fitness(crom, n, x1_l, x1_h, x2_l, x2_h):
 
     import numpy as np
 
-    x1 = (crom[0]/(10.**n))*(x1_h - x1_l) + x1_l
-    x2 = (crom[1]/(10.**n))*(x2_h - x2_l) + x2_l
+    x1 = (crom[0]/(2.**n))*(x1_h - x1_l) + x1_l
+    x2 = (crom[1]/(2.**n))*(x2_h - x2_l) + x2_l
 
     return x1*np.sin(10*np.pi*x1) + x2*np.cos(3*np.pi*(x2**2))
 
@@ -24,13 +24,17 @@ ymax = 1.8
 pop_size = 100
 
 # Maximum number of generations
-max_generation = 100
+max_generation = 1000
 
 # Maximum number of consecutive generations with no change in population
 max_unchange = 10
 
 # Precision
 digits = 6
+
+# Number of bits needed
+bits = int(np.ceil(np.log2(10**digits)))
+print bits
 
 # Mutation tax
 mutax = 0.1
@@ -46,20 +50,20 @@ specimens = []
 
 # Creating initial population with unique individuals
 for i in range(pop_size):
-    a = np.random.randint(10**digits)
-    b = np.random.randint(10**digits)
+    a = np.random.randint(2**bits)
+    b = np.random.randint(2**bits)
 
     # This loop prevents creating repeated individuals
     while [0, (a, b)] in specimens:
-        a = np.random.randint(10**digits)
-        b = np.random.randint(10**digits)
+        a = np.random.randint(2**bits)
+        b = np.random.randint(2**bits)
 
     # Add new individual to population
     specimens.append([0, (a, b)])
 
 # Fitness calculation
 for spec in specimens:
-    spec[0] = fitness(spec[1], digits, xmin, xmax, ymin, ymax,)
+    spec[0] = fitness(spec[1], bits, xmin, xmax, ymin, ymax,)
 
 # Sorting specimens for best to worst
 specimens.sort(reverse=True)
@@ -103,8 +107,8 @@ while gen < max_generation and unchange < max_unchange:
             else:
                 parent.append(specimens[champion2][1])
 
-        gene_p = [bin(parent[0][0])[2:].zfill(20) + bin(parent[0][1])[2:].zfill(20),
-                  bin(parent[1][0])[2:].zfill(20) + bin(parent[1][1])[2:].zfill(20)]
+        gene_p = [bin(parent[0][0])[2:].zfill(bits) + bin(parent[0][1])[2:].zfill(bits),
+                  bin(parent[1][0])[2:].zfill(bits) + bin(parent[1][1])[2:].zfill(bits)]
 
         # Crossover to generate child
         if len(gene_p[0]) == len(gene_p[1]):
@@ -130,10 +134,10 @@ while gen < max_generation and unchange < max_unchange:
         # print "Post-mutation:\t", ''.join(child)
 
         # Convert child from binary to int
-        child = (int('0b' + ''.join(child[:20]), 2), int('0b' + ''.join(child[20:]), 2))
+        child = (int('0b' + ''.join(child[:bits]), 2), int('0b' + ''.join(child[bits:]), 2))
 
     # Add child to last position of population
-    specimens.append([fitness(child, digits, xmin, xmax, ymin, ymax,), child])
+    specimens.append([fitness(child, bits, xmin, xmax, ymin, ymax,), child])
 
     # If child is better than worst individual, add it to population
     if specimens[-1][0] >= specimens[-2][0]:
@@ -150,10 +154,12 @@ while gen < max_generation and unchange < max_unchange:
     print 30*"-"
     for spec in specimens:
         if spec[1] == child:
-            print "\x1b[;32;49m%d: %.4f, %.4f \x1b[0m" % (specimens.index(spec) + 1, spec[1][0]/(10.**digits)*(xmax-xmin) + xmin, spec[1][1]/(10.**digits)*(ymax-ymin) + ymin)
+            print "\x1b[;32;49m%d: %.4f, %.4f \x1b[0m" % (specimens.index(spec) + 1, spec[1][0]/(2.**bits)*(xmax-xmin) + xmin, spec[1][1]/(2.**bits)*(ymax-ymin) + ymin)
         else:
-            print "%d: %.4f, %.4f" % (specimens.index(spec) + 1, spec[1][0]/(10.**digits)*(xmax-xmin) + xmin, spec[1][1]/(10.**digits)*(ymax-ymin) + ymin)
+            print "%d: %.4f, %.4f" % (specimens.index(spec) + 1, spec[1][0]/(2.**bits)*(xmax-xmin) + xmin, spec[1][1]/(2.**bits)*(ymax-ymin) + ymin)
     print "\n\n"
 
-print "Best value: %.4f, %.4f" % (specimens[0][1][0]/(10.**digits)*(xmax - xmin) + xmin, specimens[0][1][1]/(10.**digits)*(ymax - ymin) + ymin)
+print specimens
+
+print "Best value: %.4f, %.4f" % (specimens[0][1][0]/(2.**bits)*(xmax - xmin) + xmin, specimens[0][1][1]/(2.**bits)*(ymax - ymin) + ymin)
 print "Objective function: %.6f" % specimens[0][0]
